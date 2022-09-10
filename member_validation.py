4ffc8d7f9d301ffb37c30533d7c8ca1976fc5d2d
Adam <adam@planarpublishing.com>
2022-09-10 12:33:53 +0000

    Migrated from SheetDB to Google Sheets API

--- member_validation.py
+++ member_validation.py
@@ -4,10 +4,11 @@ import requests
 import os
 import re
 from dotenv import load_dotenv
-
+from sheets_api import SheetsAPI
+from googleapiclient.errors import HttpError
 
 load_dotenv()
-API_URL = os.getenv("SIGNUPS_API_URL")    
+SHEET_ID = os.getenv("SIGNUPS_SHEET_ID")    
 
 
 def valid_email(email):
@@ -19,31 +20,37 @@ def check_membership(email):
     returncode = {"status":False, "details":""}
 
     print("Checking email "+email+"...")
-    
-    r = requests.get(API_URL+"/search?sheet=signups&email="+email)
 
-    if isinstance(r.json(), list) and len(r.json()) > 0 and r.json()[0]["email"] == email:
-        print("Membership confirmed")
+    r = SheetsAPI.get_sheet_data(SHEET_ID,"signups!A2:A")
 
-        returncode["status"] = True
-        
-        # User is valid, make an entry on the discord_accounts sheet and send it through
+    try:
+        for entry in r:
+            if r[0] == email:
+                # gottem
+                
+                print("Membership confirmed")
 
-        r = requests.get(API_URL+"/search?sheet=discord_accounts&email="+email)
-        if len(r.json()) > 0 and r.json()[0]["email"] == email:
-            returncode["details"]="already_validated"
-        else:
-            print("Recording email")
-            json = { "data" : [{"email": email}]}
-            r = requests.post(API_URL+"?sheet=discord_accounts", json=json)
-            try:
-                if r.json()["created"] >= 1:
-                    print("New entry added successfully.")
-                    returncode["details"]="email_added"
+                returncode["status"] = True
+                
+                # User is valid, make an entry on the discord_accounts sheet and send it through
+
+                if email in [e[0] for e in SheetsAPI.get_sheet_data(SHEET_ID,"discord_accounts!A2:A")]:
+                    returncode["details"]="already_validated"
+                    
                 else:
-                    raise Exception("Couldn't seem to add entry:",r.json())
-            except:
-                print("Error adding new entry:",r.json())
-                returncode["details"]="error_adding_email"
-                      
+                    print("Recording email")
+
+                    try:
+                        r = SheetsAPI.append_to_sheet(SHEET_ID, "discord_accounts!A2:A", email)
+                        print("New entry added successfully.")
+                        returncode["details"]="email_added"
+                    except HttpError as e:
+                        print("Error adding new entry:",r)
+                        print("Reason:",str(e))
+                        returncode["details"]=str(e)                        
+                break
+                
+    except Exception as e:
+        returncode["details"] = str(e)
+        
     return returncode
