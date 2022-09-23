d5b992f7f401c6b16f601c40d1ae40291ecd6d8c
Adam <adam@planarpublishing.com>
2022-09-23 14:17:51 +0000

    idk??

--- member_validation.py
+++ member_validation.py
@@ -24,31 +24,29 @@ def check_membership(email):
     r = SheetsAPI.get_sheet_data(SHEET_ID,"signups!A2:A")
 
     try:
-        for entry in r:
-            if r[0] == email:
-                # gottem
+        if email in [e[0] for e in r]:
+            # gottem
+            
+            print("Membership confirmed")
+
+            returncode["status"] = True
+            
+            # User is valid, make an entry on the discord_accounts sheet and send it through
+
+            if email in [e[0] for e in SheetsAPI.get_sheet_data(SHEET_ID,"discord_accounts!A2:A")]:
+                returncode["details"]="already_validated"
                 
-                print("Membership confirmed")
+            else:
+                print("Recording email")
 
-                returncode["status"] = True
-                
-                # User is valid, make an entry on the discord_accounts sheet and send it through
-
-                if email in [e[0] for e in SheetsAPI.get_sheet_data(SHEET_ID,"discord_accounts!A2:A")]:
-                    returncode["details"]="already_validated"
-                    
-                else:
-                    print("Recording email")
-
-                    try:
-                        r = SheetsAPI.append_to_sheet(SHEET_ID, "discord_accounts!A2:A", email)
-                        print("New entry added successfully.")
-                        returncode["details"]="email_added"
-                    except HttpError as e:
-                        print("Error adding new entry:",r)
-                        print("Reason:",str(e))
-                        returncode["details"]=str(e)                        
-                break
+                try:
+                    r = SheetsAPI.append_to_sheet(SHEET_ID, "discord_accounts!A2:A", email)
+                    print("New entry added successfully.")
+                    returncode["details"]="email_added"
+                except HttpError as e:
+                    print("Error adding new entry:",r)
+                    print("Reason:",str(e))
+                    returncode["details"]=str(e)                        
                 
     except Exception as e:
         returncode["details"] = str(e)
